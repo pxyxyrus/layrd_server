93941808d9edd33d6a31500d1ee75730b5768b45
Daniel Kyutae Jung <pxyxyrus@gmail.com>
2023-07-05 00:52:26 +0000

    migration: project table update

---
+++ migrations/versions/2023_07_05_0022-f30ccd87d20c_update_project.py
@@ -0,0 +1,34 @@
+"""update project
+
+Revision ID: f30ccd87d20c
+Revises: 1081b0c2cecd
+Create Date: 2023-07-05 00:22:00.520707
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f30ccd87d20c'
+down_revision = '1081b0c2cecd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('project', sa.Column("name", sa.String(255), nullable=False))
+    op.add_column('project', sa.Column("organization", sa.String(255), nullable=False))
+    op.add_column('project', sa.Column("skills", sa.UnicodeText(), nullable=True))
+    op.drop_column('project', 'zipcode')
+    op.drop_column('project', 'optional_qualifications')
+    
+    
+
+
+def downgrade() -> None:
+    op.add_column('project', sa.Column("optional_qualifications", sa.UnicodeText(), nullable=True))
+    op.add_column("project", sa.Column("zipcode", sa.String(5), default="00000", server_default=sa.text("'00000'"), nullable=False))
+    op.drop_column('project', 'skills')
+    op.drop_column('project', 'organization')
+    op.drop_column('project', 'name')
