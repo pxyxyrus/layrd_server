6d1223eeaa4abb6bd0ec7260169ae4b3edf2be4b
Daniel Kyutae Jung <pxyxyrus@gmail.com>
2023-08-01 16:39:17 -0700

    project table migrations added

---
+++ migrations/versions/2023_08_01_1555-f74e6c0cf671_update_project.py
@@ -0,0 +1,30 @@
+"""update_project
+
+Revision ID: f74e6c0cf671
+Revises: f30ccd87d20c
+Create Date: 2023-08-01 15:55:41.313546
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f74e6c0cf671'
+down_revision = 'f30ccd87d20c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('project', sa.Column("application_number", sa.Integer(), nullable=False))
+    op.add_column('project', sa.Column("final_delivery_types", sa.String(255), nullable=False))
+    op.add_column('project', sa.Column("final_delivery_description", sa.UnicodeText(), nullable=False))
+
+    
+
+
+def downgrade() -> None:
+    op.drop_column('project', "application_number")
+    op.drop_column('project', "final_delivery_types")
+    op.drop_column('project', "final_delivery_description")
