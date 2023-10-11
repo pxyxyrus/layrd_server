0f8ccd7dd6d3e66e5b315e610dfe61579c424efe
Daniel Kyutae Jung <pxyxyrus@gmail.com>
2023-10-10 19:04:06 -0700

    fix migration

--- migrations/versions/2023_09_27_1336-dd3099ab8117_application_status_update.py
+++ migrations/versions/2023_09_27_1336-dd3099ab8117_application_status_update.py
@@ -7,7 +7,6 @@ Create Date: 2023-09-27 13:36:43.022344
 """
 from alembic import op
 import sqlalchemy as sa
-from server.types import ApplicationStatus
 
 
 
@@ -19,18 +18,50 @@ depends_on = None
 
 
 
+
 def upgrade():
     table_name = "application"
     column_name = "status"
 
+
+    from server.types import ApplicationStatus
+
     enum_types_names = ApplicationStatus._member_names_
     enum_type_string = '"' + '", "'.join(enum_types_names) + '"'
     op.execute(f"ALTER TABLE {table_name} MODIFY COLUMN {column_name} enum({enum_type_string});")
+
+    op.alter_column(
+        table_name,
+        column_name,
+        existing_type=sa.Enum(ApplicationStatus),
+        default=ApplicationStatus.applied.value,
+        server_default=sa.text("'applied'"),
+        nullable=False,
+        existing_nullable=False
+    )
    
 def downgrade():
     table_name = "application"
     column_name = "status"
+
+    # to rollback
+    import enum
+    class ApplicationStatus(enum.Enum):
+        applied = 'applied'
+        accepted = 'accepted'
+        rejected = 'rejected'
+        closed = 'closed'
     
-    enum_types_names = ApplicationStatus._member_names_[:-1]
+    enum_types_names = ApplicationStatus._member_names_
     enum_type_string = '"' + '", "'.join(enum_types_names) + '"'
     op.execute(f"ALTER TABLE {table_name} MODIFY COLUMN {column_name} enum({enum_type_string});")
+
+    op.alter_column(
+        table_name,
+        column_name,
+        existing_type=sa.Enum(ApplicationStatus),
+        default=ApplicationStatus.applied.value,
+        server_default=sa.text("'applied'"),
+        nullable=False,
+        existing_nullable=False
+    )
